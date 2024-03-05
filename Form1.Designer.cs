fa0cc3a89caef8b5f0af267ae78507e41d7868cf
Evan <Evan@WINDOWS-R6ISV9H>
2024-03-05 16:37:27 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Rolex_Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datejustLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.taxlabel = new System.Windows.Forms.Label();
+            this.subtotallabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tenderedlabel = new System.Windows.Forms.Label();
+            this.totallabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.daytonalabel = new System.Windows.Forms.Label();
+            this.pepsiLabel = new System.Windows.Forms.Label();
+            this.changebackLabel = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptInput = new System.Windows.Forms.Label();
+            this.subinputlabel = new System.Windows.Forms.Label();
+            this.totalinputLabel = new System.Windows.Forms.Label();
+            this.taxinputlabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.printButton = new System.Windows.Forms.Button();
+            this.neworderButton = new System.Windows.Forms.Button();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.daytonaInput = new System.Windows.Forms.TextBox();
+            this.DatejustInput = new System.Windows.Forms.TextBox();
+            this.pepsiInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // datejustLabel
+            // 
+            this.datejustLabel.AutoSize = true;
+            this.datejustLabel.ForeColor = System.Drawing.Color.Black;
+            this.datejustLabel.Location = new System.Drawing.Point(70, 149);
+            this.datejustLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.datejustLabel.Name = "datejustLabel";
+            this.datejustLabel.Size = new System.Drawing.Size(140, 23);
+            this.datejustLabel.TabIndex = 0;
+            this.datejustLabel.Text = "Datejust Rolex ";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(100, 410);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // taxlabel
+            // 
+            this.taxlabel.AutoSize = true;
+            this.taxlabel.Location = new System.Drawing.Point(100, 335);
+            this.taxlabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.taxlabel.Name = "taxlabel";
+            this.taxlabel.Size = new System.Drawing.Size(40, 23);
+            this.taxlabel.TabIndex = 3;
+            this.taxlabel.Text = "Tax";
+            // 
+            // subtotallabel
+            // 
+            this.subtotallabel.AutoSize = true;
+            this.subtotallabel.Location = new System.Drawing.Point(100, 286);
+            this.subtotallabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.subtotallabel.Name = "subtotallabel";
+            this.subtotallabel.Size = new System.Drawing.Size(90, 23);
+            this.subtotallabel.TabIndex = 4;
+            this.subtotallabel.Text = "Sub Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1152, 503);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(250, 853);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // tenderedlabel
+            // 
+            this.tenderedlabel.AutoSize = true;
+            this.tenderedlabel.Location = new System.Drawing.Point(100, 459);
+            this.tenderedlabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.tenderedlabel.Name = "tenderedlabel";
+            this.tenderedlabel.Size = new System.Drawing.Size(85, 23);
+            this.tenderedlabel.TabIndex = 7;
+            this.tenderedlabel.Text = "Tendered";
+            // 
+            // totallabel
+            // 
+            this.totallabel.AutoSize = true;
+            this.totallabel.Location = new System.Drawing.Point(100, 378);
+            this.totallabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.totallabel.Name = "totallabel";
+            this.totallabel.Size = new System.Drawing.Size(53, 23);
+            this.totallabel.TabIndex = 8;
+            this.totallabel.Text = "Total";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(345, 23);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(200, 23);
+            this.titleLabel.TabIndex = 9;
+            this.titleLabel.Text = "Fake Rolex Emporium";
+            // 
+            // daytonalabel
+            // 
+            this.daytonalabel.AutoSize = true;
+            this.daytonalabel.Location = new System.Drawing.Point(76, 196);
+            this.daytonalabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.daytonalabel.Name = "daytonalabel";
+            this.daytonalabel.Size = new System.Drawing.Size(134, 23);
+            this.daytonalabel.TabIndex = 10;
+            this.daytonalabel.Text = "Daytona Rolex";
+            // 
+            // pepsiLabel
+            // 
+            this.pepsiLabel.AutoSize = true;
+            this.pepsiLabel.ForeColor = System.Drawing.Color.Black;
+            this.pepsiLabel.Location = new System.Drawing.Point(100, 104);
+            this.pepsiLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.pepsiLabel.Name = "pepsiLabel";
+            this.pepsiLabel.Size = new System.Drawing.Size(110, 23);
+            this.pepsiLabel.TabIndex = 11;
+            this.pepsiLabel.Text = "Pepsi Rolex";
+            // 
+            // changebackLabel
+            // 
+            this.changebackLabel.Location = new System.Drawing.Point(232, 569);
+            this.changebackLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.changebackLabel.Name = "changebackLabel";
+            this.changebackLabel.Size = new System.Drawing.Size(85, 23);
+            this.changebackLabel.TabIndex = 12;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(100, 569);
+            this.changeLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(72, 23);
+            this.changeLabel.TabIndex = 13;
+            this.changeLabel.Text = "Change";
+            // 
+            // receiptInput
+            // 
+            this.receiptInput.BackColor = System.Drawing.SystemColors.Control;
+            this.receiptInput.Location = new System.Drawing.Point(370, 101);
+            this.receiptInput.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.receiptInput.Name = "receiptInput";
+            this.receiptInput.Size = new System.Drawing.Size(426, 491);
+            this.receiptInput.TabIndex = 14;
+            // 
+            // subinputlabel
+            // 
+            this.subinputlabel.Location = new System.Drawing.Point(232, 286);
+            this.subinputlabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.subinputlabel.Name = "subinputlabel";
+            this.subinputlabel.Size = new System.Drawing.Size(40, 23);
+            this.subinputlabel.TabIndex = 15;
+            // 
+            // totalinputLabel
+            // 
+            this.totalinputLabel.Location = new System.Drawing.Point(232, 378);
+            this.totalinputLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.totalinputLabel.Name = "totalinputLabel";
+            this.totalinputLabel.Size = new System.Drawing.Size(40, 23);
+            this.totalinputLabel.TabIndex = 16;
+            // 
+            // taxinputlabel
+            // 
+            this.taxinputlabel.Location = new System.Drawing.Point(232, 335);
+            this.taxinputlabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.taxinputlabel.Name = "taxinputlabel";
+            this.taxinputlabel.Size = new System.Drawing.Size(40, 23);
+            this.taxinputlabel.TabIndex = 17;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(104, 519);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(230, 29);
+            this.changeButton.TabIndex = 18;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // totalButton
+            // 
+            this.totalButton.Location = new System.Drawing.Point(104, 242);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(230, 29);
+            this.totalButton.TabIndex = 19;
+            this.totalButton.Text = "Calculate Total";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(104, 624);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(230, 29);
+            this.printButton.TabIndex = 20;
+            this.printButton.Text = "Print Receipt";
+            this.printButton.UseVisualStyleBackColor = true;
+            // 
+            // neworderButton
+            // 
+            this.neworderButton.Location = new System.Drawing.Point(374, 624);
+            this.neworderButton.Name = "neworderButton";
+            this.neworderButton.Size = new System.Drawing.Size(422, 29);
+            this.neworderButton.TabIndex = 21;
+            this.neworderButton.Text = "New Order";
+            this.neworderButton.UseVisualStyleBackColor = true;
+            this.neworderButton.Click += new System.EventHandler(this.neworderButton_Click);
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(206, 459);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(100, 32);
+            this.tenderedInput.TabIndex = 22;
+            // 
+            // daytonaInput
+            // 
+            this.daytonaInput.Location = new System.Drawing.Point(219, 196);
+            this.daytonaInput.Name = "daytonaInput";
+            this.daytonaInput.Size = new System.Drawing.Size(100, 32);
+            this.daytonaInput.TabIndex = 23;
+            // 
+            // DatejustInput
+            // 
+            this.DatejustInput.Location = new System.Drawing.Point(219, 146);
+            this.DatejustInput.Name = "DatejustInput";
+            this.DatejustInput.Size = new System.Drawing.Size(100, 32);
+            this.DatejustInput.TabIndex = 24;
+            // 
+            // pepsiInput
+            // 
+            this.pepsiInput.Location = new System.Drawing.Point(219, 101);
+            this.pepsiInput.Name = "pepsiInput";
+            this.pepsiInput.Size = new System.Drawing.Size(100, 32);
+            this.pepsiInput.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(811, 659);
+            this.Controls.Add(this.pepsiInput);
+            this.Controls.Add(this.DatejustInput);
+            this.Controls.Add(this.daytonaInput);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.neworderButton);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.taxinputlabel);
+            this.Controls.Add(this.totalinputLabel);
+            this.Controls.Add(this.subinputlabel);
+            this.Controls.Add(this.receiptInput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changebackLabel);
+            this.Controls.Add(this.pepsiLabel);
+            this.Controls.Add(this.daytonalabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.totallabel);
+            this.Controls.Add(this.tenderedlabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.subtotallabel);
+            this.Controls.Add(this.taxlabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.datejustLabel);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label datejustLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label taxlabel;
+        private System.Windows.Forms.Label subtotallabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label tenderedlabel;
+        private System.Windows.Forms.Label totallabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label daytonalabel;
+        private System.Windows.Forms.Label pepsiLabel;
+        private System.Windows.Forms.Label changebackLabel;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label receiptInput;
+        private System.Windows.Forms.Label subinputlabel;
+        private System.Windows.Forms.Label totalinputLabel;
+        private System.Windows.Forms.Label taxinputlabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Button neworderButton;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.TextBox daytonaInput;
+        private System.Windows.Forms.TextBox DatejustInput;
+        private System.Windows.Forms.TextBox pepsiInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Rolex_Shop
+{
+    public partial class Form1 : Form
+    {
+        //global Varriables
+        double pepsiPrice = 100.25;
+        double datejustPrice = 200.75;
+        double daytonaPrice = 150.75;
+        int numPepsi;
+        int numDatejust;
+        int numDaytona;
+        double tendered;
+        double money;
+        double pepsiReceipt;
+        double daytonaReceipt;
+        double datejustReceipt;
+
+        SoundPlayer error;
+        public Form1()
+        {
+            InitializeComponent();
+            printButton.Visible = false;
+            //error = new SoundPlayer(Properties.Resources.ERROR); havent added sound of a cash register yet if you could do that.
+            titleLabel.Visible = false;
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            //when total button is pressed, convert the input to integers, calculate the subtotal, tax and total price.
+            //Catch error when user dont enter a number or enter a text message...
+            try
+            {
+                numPepsi = Convert.ToInt16(pepsiInput.Text);
+                //numDatejust = Convert.ToInt16(dateInput.Text); cant get it working
+                numDaytona = Convert.ToInt16(daytonaInput.Text);
+
+
+                double banhmi = numPepsi * pepsiPrice;
+                double ramen = numDatejust * datejustPrice;
+                double boba = numDaytona * daytonaPrice;
+
+                //pepsiReceipt = pepsi;
+                //datejustReceipt = datejust; cant get it working
+                //daytonaReceipt = daytona;
+
+                double subTotalPrice = (banhmi) + (ramen) + (boba);
+                double tax = subTotalPrice * 0.13;
+                double totalPrice = subTotalPrice + tax;
+                money = totalPrice;
+
+                //subinputLabel.Text = subTotalPrice.ToString("0.00");
+                //taxinputLabel.Text = tax.ToString("0.00");
+                //totalinputLabel.Text = totalPrice.ToString("0.00"); not working
+            }
+            catch
+            {
+                subinputlabel.Text = $"Error";
+                taxinputlabel.Text = $"Error";
+                //totalinputlabel.Text = $"Error"; not working
+                error.Play();
+            }
+
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            // when calculate change button is pressed, convert the tendered input to double, calculate the change
+            // if the tendered money is lower than the total = error
+            try
+            {
+                double changeInput = Convert.ToInt16(tenderedInput.Text);
+                double change = (changeInput) - (money);
+                changebackLabel.Text = change.ToString("0.00");
+
+                if (change < 0)
+                {
+                    changebackLabel.Text = $"U r broke";
+
+                }
+
+                printButton.Visible = true;
+            }
+            catch
+            {
+                changebackLabel.Text = $"ERROR";
+                error.Play();
+            }
+
+
+        }
+
+
+
+        private void receiptButton_Click(object sender, EventArgs e)
+
+        // when receipt button click, print receipt.
+        {
+            try
+            {
+                tendered = Convert.ToDouble(tenderedInput.Text);
+                //SoundPlayer cashPlay = new SoundPlayer(Properties.Resources.receiptSound); sound required
+                //cashPlay.Play();
+                receiptInput.Text += $"                  {titleLabel.Text}";
+
+                //cashPlay.Play();
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{line.Text}"; not sure
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{line.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{numDatejust}           {datejustLabel.Text}                       ${datejusteceipt.ToString("0.00")}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+               ///* receiptInput.Text += $"\n{numDaytona}  not sure         {daytonaLabel.Text} */            ${daytonaReceipt.ToString("0.00")}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{numPepsi}           {pepsiLabel.Text}                          /*   ${pepsiReceipt.ToString("0.00")}";*/
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{line.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                receiptInput.Text += $"\n{subinputlabel.Text}                                       ${subinputlabel.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                receiptInput.Text += $"\n{taxinputlabel.Text}                                                 ${taxinputlabel.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{totalinputlabel.Text}                                              ${totalinputLabel.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{line.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                receiptInput.Text += $"\n{tenderedInput.Text}                                       ${tendered.ToString("0.00")}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{changeLabel.Text}                                            ${changeInput.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                //receiptInput.Text += $"\n{line.Text}";
+
+                this.Refresh();
+                Thread.Sleep(500);
+
+                receiptInput.Text += $"\n                            Thank You!";
+
+                this.Refresh();
+                Thread.Sleep(500);
+                receiptInput.Text += $"\n                         Have a nice day!!";
+                /*receiptLabel.Text += $"\n */                          /*23rd Feb 2024";*/
+            }
+            catch
+            {
+                receiptInput.Text = $"ERROR";
+                error.Play();
+            }
+
+
+
+
+
+
+
+
+        }
+
+        private void neworderButton_Click(object sender, EventArgs e)
+        {
+            //when new order button is clicked reset the input to 0, reset all the label to " " (empty).
+            numPepsi = 0;
+            numDatejust = 0;
+            numDaytona = 0;
+            tendered = 0;
+
+            //datejustInput.Text = " ";
+            daytonaInput.Text = " ";
+            pepsiInput.Text = " ";
+            //subinputLabel.Text = " ";
+            taxinputlabel.Text = " ";
+            //totalinputlabel.Text = " ";
+            tenderedInput.Text = " ";
+            changebackLabel.Text = " ";
+            receiptInput.Text = " ";
+
+
+
+
+        }
+    }
+    }
+    }
+    }
+}
